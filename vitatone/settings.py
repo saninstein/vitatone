38ab3090527b8d129e961653aa3a9bdb6d038cda
Alex <saninstein@yandex.ua>
2017-12-06 22:43:08 +0300

    fixes

--- vitatone/settings.py
+++ vitatone/settings.py
@@ -78,25 +78,27 @@ WSGI_APPLICATION = 'vitatone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vitatone',
-        'USER': 'user',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    },
-
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 if 'PREPROD' in os.environ:
-    DATABASES['default'] = DATABASES['sqlite']
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'vitatone',
+            'USER': 'user',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
+
+
+
 
 
 # Password validation
