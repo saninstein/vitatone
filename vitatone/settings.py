ba667f663f70f7d02b3b3531446c5197cc1a8302
Alex <saninstein@yandex.ua>
2017-12-06 22:39:48 +0300

    fixes

--- vitatone/settings.py
+++ vitatone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6x(81&8rjpup29=hgv+h+@rs-m#3%ab=rg5(br7v56nws6mugg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
